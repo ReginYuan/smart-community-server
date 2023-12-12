1723c5c03f270c58bc85c8b1d9779e79b05b140b
ReginYuan <2496943860@qq.com>
2023-12-12 22:05:13 +0800

    feat: 修改代码

--- app.js
+++ app.js
@@ -1,74 +1,80 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const log4js = require('./utils/log4j')
-const router = require('koa-router')()
-const jwt = require('jsonwebtoken')
-const koajwt = require('koa-jwt')
-const util = require('./utils/util')
-const users = require('./routes/users')
-const menus = require('./routes/menus')
-const roles = require('./routes/roles')
-const depts = require('./routes/depts')
-const leave = require('./routes/leave')
-const face = require('./routes/face')
-const device = require('./routes/device')
-const faceid = require('./routes/faceid')
+const Koa = require("koa");
+const app = new Koa();
+const views = require("koa-views");
+const json = require("koa-json");
+const onerror = require("koa-onerror");
+const bodyparser = require("koa-bodyparser");
+const logger = require("koa-logger");
+const log4js = require("./utils/log4j");
+const router = require("koa-router")();
+const jwt = require("jsonwebtoken");
+const koajwt = require("koa-jwt");
+const util = require("./utils/util");
+const users = require("./routes/users");
+const menus = require("./routes/menus");
+const roles = require("./routes/roles");
+const depts = require("./routes/depts");
+const leave = require("./routes/leave");
+const face = require("./routes/face");
+const device = require("./routes/device");
+const faceid = require("./routes/faceid");
 // error handler
-onerror(app)
+onerror(app);
 
-require('./config/db')
-require('./config/clickhouse')
+require("./config/db");
+require("./config/clickhouse");
 
 // middlewares
-app.use(bodyparser({
-  enableTypes: ['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(
+  bodyparser({
+    enableTypes: ["json", "form", "text"]
+  })
+);
+app.use(json());
+app.use(logger());
+app.use(require("koa-static")(__dirname + "/public/dist"));
 
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
+app.use(
+  views(__dirname + "/views", {
+    extension: "pug"
+  })
+);
 
 // logger
 app.use(async (ctx, next) => {
-  log4js.info(`get params:${JSON.stringify(ctx.request.query)}`)
-  log4js.info(`post params:${JSON.stringify(ctx.request.body)}`)
+  log4js.info(`get params:${JSON.stringify(ctx.request.query)}`);
+  log4js.info(`post params:${JSON.stringify(ctx.request.body)}`);
   await next().catch((err) => {
-    if (err.status == '401') {
+    if (err.status == "401") {
       ctx.status = 200;
-      ctx.body = util.fail('Token认证失败', util.CODE.AUTH_ERROR)
+      ctx.body = util.fail("Token认证失败", util.CODE.AUTH_ERROR);
     } else {
       throw err;
     }
-  })
-})
-
-app.use(koajwt({ secret: 'imooc' }).unless({
-  path: [/^\/api\/users\/login/]
-}))
-
-router.prefix("/api")
-
-router.use(users.routes(), users.allowedMethods())
-router.use(menus.routes(), menus.allowedMethods())
-router.use(roles.routes(), roles.allowedMethods())
-router.use(depts.routes(), depts.allowedMethods())
-router.use(leave.routes(), leave.allowedMethods())
-router.use(face.routes(), face.allowedMethods())
-router.use(device.routes(), device.allowedMethods())
-router.use(faceid.routes(), faceid.allowedMethods())
-app.use(router.routes(), router.allowedMethods())
-
-// error-handling
-app.on('error', (err, ctx) => {
-  log4js.error(`${err.stack}`)
+  });
 });
 
-module.exports = app
+app.use(
+  koajwt({ secret: "imooc" }).unless({
+    path: [/^\/api\/users\/login/]
+  })
+);
+
+router.prefix("/api");
+
+router.use(users.routes(), users.allowedMethods());
+router.use(menus.routes(), menus.allowedMethods());
+router.use(roles.routes(), roles.allowedMethods());
+router.use(depts.routes(), depts.allowedMethods());
+router.use(leave.routes(), leave.allowedMethods());
+router.use(face.routes(), face.allowedMethods());
+router.use(device.routes(), device.allowedMethods());
+router.use(faceid.routes(), faceid.allowedMethods());
+app.use(router.routes(), router.allowedMethods());
+
+// error-handling
+app.on("error", (err, ctx) => {
+  log4js.error(`${err.stack}`);
+});
+
+module.exports = app;
